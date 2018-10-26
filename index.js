b870d7bfb8dd634dee9636ca382cff6a755532d7
Alois Barreras <aloisbarreras@icloud.com>
2018-10-26 17:07:16 -0400

    adding support for render and cookies

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 module.exports = fn => async (req, res, next, ...args) => {
   try {
     const result = await fn(req, res, next, ...args);
-    const { statusCode: status = 200, body, headers = {} } = result;
+    const {
+      statusCode: status = 200,
+      body,
+      headers,
+      cookies = [],
+      render,
+      renderLocals,
+    } = result;
+
     res.status(status);
-    res.set(headers);
+    if (headers) res.set(headers);
+
+    cookies.forEach(x => res.cookie(x.name, x.value, x.options));
+
+    if (render) return res.render(render, renderLocals);
     if (body) return res.send(body);
     return res.send();
   } catch (e) {
--- index.test.js
+++ index.test.js
@@ -1,6 +1,7 @@
 const chai = require('chai');
 const { expect } = require('chai');
 const sinon = require('sinon');
+const sandbox = require('sinon').createSandbox();
 const sinonChai = require('sinon-chai');
 const express = require('express');
 const request = require('supertest');
@@ -9,87 +10,122 @@ chai.use(sinonChai);
 const asyncController = require('./index');
 
 describe('express-controller', () => {
+  let statusSpy;
+  let setSpy;
+  let cookieSpy;
+  let renderSpy;
+  let sendSpy;
+  let nextSpy;
+
+  beforeEach(() => {
+    statusSpy = sandbox.spy();
+    setSpy = sandbox.spy();
+    cookieSpy = sandbox.spy();
+    renderSpy = sandbox.spy();
+    sendSpy = sandbox.spy();
+    nextSpy = sandbox.spy();
+  });
+
+  afterEach(() => {
+    sandbox.restore();
+  });
+
   it('should call next with synchronous errors', async () => {
     const error = new Error('catch me!');
-    const spy = sinon.spy();
     const foo = asyncController(() => {
       throw error;
     });
 
-    await foo(null, null, spy);
-    expect(spy).to.have.been.calledWith(error);
+    await foo(null, null, nextSpy);
+    expect(nextSpy).to.have.been.calledWith(error);
   });
 
   it('should call next with async errors', async () => {
     const error = new Error('catch me!');
-    const spy = sinon.spy();
     const foo = asyncController(async () => {
       throw error;
     });
 
-    await foo(null, null, spy);
-    expect(spy).to.have.been.calledWith(error);
+    await foo(null, null, nextSpy);
+    expect(nextSpy).to.have.been.calledWith(error);
   });
 
   it('should call next with the arguments when an async function passed into it calls next', async () => {
-    const spy = sinon.spy();
     const foo = asyncController(async (req, res, next) => next('test'));
 
-    await foo(null, null, spy);
-    expect(spy).to.have.been.calledWith('test');
+    await foo(null, null, nextSpy);
+    expect(nextSpy).to.have.been.calledWith('test');
   });
 
   it('should provide additional arguments to the middleware', async () => {
-    const spy = sinon.spy();
     const id = '1';
     const foo = asyncController(async (req, res, next, ID) => ({ body: { id: ID } }));
 
-    await foo(null, { status: spy, set: spy, send: spy }, null, id);
-    expect(spy).to.have.been.calledWith(200); // default status
-    expect(spy).to.have.been.calledWith({}); // no headers returned
-    expect(spy).to.have.been.calledWith({ id });
+    await foo(null, { status: statusSpy, send: sendSpy }, null, id);
+    expect(statusSpy).to.have.been.calledWith(200); // default status
+    expect(sendSpy).to.have.been.calledWith({ id });
   });
 
   it('should call res.status and res.headers and res.send with correct values', async () => {
-    const spy = sinon.spy();
     const id = '1';
     const headers = { 'X-My-Header': 'hello' };
     const foo = asyncController(async (req, res, next, ID) =>
       ({ statusCode: 201, headers, body: { id: ID } })); // eslint-disable-line implicit-arrow-linebreak
 
-    await foo(null, { status: spy, set: spy, send: spy }, null, id);
-    expect(spy).to.have.been.calledWith(201);
-    expect(spy).to.have.been.calledWith(headers);
-    expect(spy).to.have.been.calledWith({ id });
+    await foo(null, { status: statusSpy, set: setSpy, send: sendSpy }, null, id);
+    expect(statusSpy).to.have.been.calledWith(201);
+    expect(setSpy).to.have.been.calledWith(headers);
+    expect(sendSpy).to.have.been.calledWith({ id });
   });
 
-  let app;
-  let spy;
-  let error;
-  const body = { hello: 'world' };
+  it('should call res.cookie with correct values', async () => {
+    const cookies = [{ name: 'rememberme', value: 1 }];
+    const foo = asyncController(async () =>
+      ({ statusCode: 201, cookies })); // eslint-disable-line implicit-arrow-linebreak
 
-  beforeEach(() => {
-    error = new Error('Oops!');
-    spy = sinon.spy();
-    app = express();
-    app.all('/', asyncController(async () => ({ statusCode: 302, body })));
-    app.all('/error', asyncController(async () => { throw error; }));
-    app.use((err, req, res, next) => {
-      spy(err);
-      res.send();
+    await foo(null, { status: statusSpy, cookie: cookieSpy, send: sendSpy }, null);
+    expect(statusSpy).to.have.been.calledWith(201);
+    expect(cookieSpy).to.have.been.calledWith(cookies[0].name, cookies[0].value);
+    expect(sendSpy).to.have.been.called;
+  });
+
+  it('should call res.render with correct values', async () => {
+    const foo = asyncController(async () => ({ statusCode: 201, render: 'sign-up', renderLocals: { foo: 'bar' } }));
+
+    await foo(null, { status: statusSpy, render: renderSpy }, null);
+    expect(statusSpy).to.have.been.calledWith(201);
+    expect(renderSpy).to.have.been.calledOnceWithExactly('sign-up', { foo: 'bar' });
+  });
+
+  describe('express', () => {
+    let app;
+    let spy;
+    let error;
+    const body = { hello: 'world' };
+
+    beforeEach(() => {
+      error = new Error('Oops!');
+      spy = sinon.spy();
+      app = express();
+      app.all('/', asyncController(async () => ({ statusCode: 302, body })));
+      app.all('/error', asyncController(async () => { throw error; }));
+      app.use((err, req, res, next) => {
+        spy(err);
+        res.send();
+      });
     });
-  });
 
-  it('should work with express routes', async () => {
-    const result = await request(app)
-      .get('/')
-      .expect(302);
-    expect(result.body).to.deep.equal(body);
-  });
+    it('should work with express routes', async () => {
+      const result = await request(app)
+        .get('/')
+        .expect(302);
+      expect(result.body).to.deep.equal(body);
+    });
 
-  it('should go to the error handler', async () => {
-    await request(app)
-      .get('/error');
-    expect(spy).to.have.been.calledWith(error);
+    it('should go to the error handler', async () => {
+      await request(app)
+        .get('/error');
+      expect(spy).to.have.been.calledWith(error);
+    });
   });
 });
